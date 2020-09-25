19340228e089340ab65f2b5daf48bd8a064c8125
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-09-25 19:01:50 +0700

    Update Tu_dien.java

--- Tu_dien.java
+++ Tu_dien.java
@@ -39,11 +39,11 @@ public class Tu_dien {
             Scanner in = new Scanner(System.in);
             String en, vi;
             /**Nhap dong tieng anh */
-            en=in.nextLine();
+            en = in.nextLine();
             /**Nhap dong tieng viet*/
-            vi=in.nextLine();
+            vi = in.nextLine();
 
-            String c="";
+            String c = "";
                 /**Doc cac tu tieng anh tu dong vua nhap*/
                 for(int j = 0; j < en.length(); j++) {
                     if(!Character.isSpace(en.charAt(j))) {
