8e275cdff690a0f174220f11cd8e89871b4acdb8
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-09-25 18:59:50 +0700

    Update Tu_dien.java

--- Tu_dien.java
+++ Tu_dien.java
@@ -3,57 +3,97 @@ import java.util.Scanner;
 import java.util.Vector;
 /**sdasad.*/
 public class Tu_dien {
-    public class Word{
-         String word_target, word_explain;
+    Vector<String> a = new Vector<>();
+    Vector<String> b = new Vector<>();
+    
+    /** class Word .*/
+    public class Word {
+         private String word_target, word_explain;
+
+        public String getWord_explain() {
+            return word_explain;
+        }
+
+        public String getWord_target() {
+            return word_target;
+        }
+
+        public void setWord_explain(String word_explain) {
+            this.word_explain = word_explain;
+        }
+
+        public void setWord_target(String word_target) {
+            this.word_target = word_target;
+        }
+    }
+    
+    /** class Dictionary lưu trữ mảng Word .*/
+    public class Dictionary {
+        Vector<Word> danhsach =  new Vector<>();
 
     }
-    public class Dictionary{
-        String[] Words;
-    }
-    public class DictionaryManagement{
-        String[] insertfromCommandline(String[] args){
+    
+    /** class có hàm Insert có chức năng nhập liệu */
+    public class DictionaryManagement {
+        void insertfromCommandline() {
             Scanner in = new Scanner(System.in);
-            String en,vi;
+            String en, vi;
             /**Nhap dong tieng anh */
             en=in.nextLine();
             /**Nhap dong tieng viet*/
             vi=in.nextLine();
-            Vector<String> a = new Vector<>();
-            Vector<String> b = new Vector<>();
+
             String c="";
                 /**Doc cac tu tieng anh tu dong vua nhap*/
-                for(int j=0;j<en.length();j++) {
-                    if(!Character.isSpace(en.charAt(j))){
-                        c+=en.charAt(j);
+                for(int j = 0; j < en.length(); j++) {
+                    if(!Character.isSpace(en.charAt(j))) {
+                        c += en.charAt(j);
                     }
                     else {
                         a.add(c);
-                        c="";
+                        c = "";
                         continue;
                     }
 
                 }
                 /**Doc cac tu tieng viet tu dong vua nhap*/
-            for(int j=0;j<en.length();j++) {
+            for(int j = 0; j < en.length(); j++) {
                 if(!Character.isSpace(en.charAt(j))){
-                    c+=en.charAt(j);
+                    c += en.charAt(j);
                 }
                 else {
                     a.add(c);
-                    c="";
+                    c = "";
                     continue;
                 }
 
             }
-            return args;
-        }
-    }
-    public class DictionaryCommandline{
-        void showAllWords(){
 
         }
-        void dictionaryBasic(){
 
+    }
+    
+    /** class có hàm ShowAllWords hiển thị các từ đã nhập */
+    public class DictionaryCommandline {
+        String hienthi(String a, int n) {
+            String space = a;
+            for(int i = 0; i < n - a.length(); i++) {
+                space += " ";
+            }
+            return a;
+        }
+        void showAllWords() {
+            System.out.println("No            | English                   | Vietnamese                ");
+            for(int i = 0;i <a.size(); i++) {
+            System.out.println(i + 1 + "            | " + hienthi(a.get(i), 10) + " | " + hienthi(b.get(i), 10)  );
+            }
+        }
+        void dictionaryBasic() {
+        DictionaryManagement insert = new DictionaryManagement();
+        insert.insertfromCommandline();
+        showAllWords();
+        
         }
     }
+
 }
