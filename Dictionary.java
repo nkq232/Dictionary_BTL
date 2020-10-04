74e5b978cbd79239a020b9ff79aeff9e754b7f78
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-04 16:23:45 +0700

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,9 +1,15 @@
-package Dictionary;
-
+package Tu_dien;
 import java.util.ArrayList;
-
-/** class Dictionary lưu trữ mảng Word .*/
 public class Dictionary {
-    ArrayList<Word> tu = new ArrayList<Word>();
+    private ArrayList<Word> list;
+    public Dictionary() {
+        list = new ArrayList<Word>();
 
+    }
+    public ArrayList<Word> getList() {
+        return list;
+    }
+    public Dictionary(ArrayList<Word> a) {
+        this.list = a;
+    }
 }
