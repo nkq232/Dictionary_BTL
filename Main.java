277ac1850ae2932ab77f13c0f3391fc0f740921b
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-04 16:26:27 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
-package Dictionary;
+package Tu_dien;
 
 import java.util.Scanner;
-import java.util.ArrayList;
-/**sdasad.*/
+
 public class Main {
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Dictionary tu_dien = new Dictionary();
+        DictionaryManagement nhap = new DictionaryManagement(tu_dien);
+        nhap.insertFromCommandline(tu_dien.getList());
+        DictionaryCommandline test = new DictionaryCommandline(tu_dien,nhap);
+        test.dictionaryBasic();
     }
+
 }
