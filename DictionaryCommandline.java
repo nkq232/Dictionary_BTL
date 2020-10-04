dc0bfd94aba189e36fd3774f4135c45d7acde7dc
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-04 16:25:28 +0700

    Update DictionaryCommandline.java

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,24 +1,49 @@
-package Dictionary;
+package Tu_dien;
 
-/** class có hàm ShowAllWords hiển thị các từ đã nhập */
+import java.util.ArrayList;
+
 public class DictionaryCommandline {
+    private Dictionary dictionary;
+    private DictionaryManagement dict_manage;
+    private ArrayList<Word> list;
+
+    // Constructor
+    public DictionaryCommandline(Dictionary dictionary) {
+        this.dictionary = dictionary;
+        dict_manage = new DictionaryManagement(dictionary);
+    }
+    public DictionaryCommandline(Dictionary dictionary, DictionaryManagement dict_manage) {
+        this.dictionary = dictionary;
+        this.dict_manage = dict_manage;
+    }
+    public DictionaryCommandline() {
+
+    }
+    // End Constructor
     String hienthi(String a, int n) {
-        String space = a;
+        StringBuilder space = new StringBuilder(a);
         for(int i = 0; i < n - a.length(); i++) {
-            space += " ";
+            space.append(" ");
         }
         return a;
     }
-    void showAllWords() {
-        System.out.println("No            | English                   | Vietnamese                ");
-        for(int i = 0;i <a.size(); i++) {
-            System.out.println(i + 1 + "            | " + hienthi(a.get(i), 10) + " | " + hienthi(b.get(i), 10)  );
+    public void showWord(Word a) {
+        System.out.println(a.getWord_target() + hienthi(a.getWord_target(),12)
+                + "| " + a.getWord_explain());
+    }
+    public void showAllWords() {
+        for(int i = 0; i < list.size(); i++) {
+            System.out.print(i + "    |");
+            showWord(list.get(i));
         }
     }
-    void dictionaryBasic() {
-        DictionaryManagement insert = new DictionaryManagement();
-        insert.insertfromCommandline();
+    public void dictionaryBasic() {
+        dict_manage.insertFromCommandline(dictionary.getList());
         showAllWords();
-
+    }
+    public void dictionaryAdvanced() throws Exception {
+        dict_manage.insertFromFile();
+        showAllWords();
+        dict_manage.dictionaryLookup(list);
     }
 }
