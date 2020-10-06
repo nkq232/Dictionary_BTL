60feda7be75463d87cde737be940573890ecbe06
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-06 21:15:53 +0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,29 +1,47 @@
 package Tu_dien;
 
+import java.util.ArrayList;
+
 public class Word {
     private String word_target;
-    private String word_explain;
+    private ArrayList<String> word_explain;
+    //Constructors
     public Word(String word_target, String word_explain) {
-        this.word_explain = word_explain;
+        this.word_explain.add(word_explain);
         this.word_target = word_target;
     }
     public Word(){
-
     }
 
+    //Constructors
+
+    //Getter and Setter
     public String getWord_target() {
+        
         return word_target;
     }
-
-    public void setWord_target(String word_target) {
-        this.word_target = word_target;
-    }
-
-    public String getWord_explain() {
+    public ArrayList<String> getWord_explain() {
+        
         return word_explain;
     }
-
-    public void setWord_explain(String word_explain) {
-        this.word_explain = word_explain;
+    public void setWord_target(String word_target) {
+        
+        this.word_target = word_target;
     }
+    public void setWord_explain(ArrayList<String> a) {
+        
+        word_explain.addAll(a);
+    }
+    //Getter and Setter
+
+    //More functions
+    public void addWord_explain(String a) {
+        if(word_explain == null) word_explain = new ArrayList<String>();
+        word_explain.add(a);
+    }
+    public void copy(Word a) {
+        this.setWord_explain(a.getWord_explain());
+        this.setWord_target(a.getWord_target());
+    }
+
 }
