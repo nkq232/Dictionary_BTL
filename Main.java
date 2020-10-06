872a1b7178beaca429e0d2de9a4d7e54c544b610
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-06 22:35:37 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Dictionary tu_dien = new Dictionary();
-        DictionaryManagement nhap = new DictionaryManagement(tu_dien);
-        nhap.insertFromCommandline(tu_dien.getList());
-        DictionaryCommandline test = new DictionaryCommandline(tu_dien,nhap);
-        test.dictionaryBasic();
+        
     }
 
 }
