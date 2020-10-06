ae9270514c36591cbffd42815af7ea7c0e8d76ac
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-06 21:43:14 +0700

    Update DictionaryCommandline.java

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,5 +1,7 @@
 package Tu_dien;
-
+import java.util.*;
+import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class DictionaryCommandline {
@@ -20,30 +22,57 @@ public class DictionaryCommandline {
 
     }
     // End Constructor
-    String hienthi(String a, int n) {
-        StringBuilder space = new StringBuilder(a);
-        for(int i = 0; i < n - a.length(); i++) {
-            space.append(" ");
-        }
-        return a;
-    }
-    public void showWord(Word a) {
-        System.out.println(a.getWord_target() + hienthi(a.getWord_target(),12)
-                + "| " + a.getWord_explain());
-    }
     public void showAllWords() {
-        for(int i = 0; i < list.size(); i++) {
-            System.out.print(i + "    |");
-            showWord(list.get(i));
+        if(list == null) {
+            list = new ArrayList<Word>(dictionary.getList());
         }
+        boolean check_Empty = true;
+        int thu_tu = 0;
+        for(int i = 0; i < list.size(); i++) {
+            if (list.get(i) == null) continue;
+            for(int j = 0; j < list.get(i).getWord_explain().size(); j++) {
+                if (check_Empty) {
+                    check_Empty = false;
+                    String a = "No";
+                    String b = "English";
+                    String c = "Vietnamese";
+                    System.out.printf("%-8s|%-40s|%s%n%n", a, b, c);
+                }
+                System.out.printf("%-8d|%-40s|%s%n", ++thu_tu, list.get(i).getWord_target(), list.get(i).getWord_explain().get(j));
+            }
+        }
+        if (check_Empty) {
+            System.out.println("There are no words in dictionary");
+        }
+        list = null;
     }
     public void dictionaryBasic() {
-        dict_manage.insertFromCommandline(dictionary.getList());
+        dict_manage.insertFromCommandline();
         showAllWords();
     }
     public void dictionaryAdvanced() throws Exception {
         dict_manage.insertFromFile();
         showAllWords();
-        dict_manage.dictionaryLookup(list);
+        dict_manage.dictionaryLookup();
+    }
+    public void DictionarySearcher() {
+        Scanner in = new Scanner(System.in);
+    System.out.println("Enter to find : ");
+    String find = in.nextLine();
+    ArrayList<String> result = new ArrayList<String>();
+    result = dictionary.Searcher(find);
+    if (result == null) {
+        System.out.println("We cant find anything like that");
+    }
+    else if (result.size() == 0) {
+        System.out.println("We cant find anything like that");
+    }
+    else {
+        System.out.println("We found" + result.size() + " results");
+        for(int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
     }
 }
+
