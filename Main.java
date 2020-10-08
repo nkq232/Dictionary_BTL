1d1bcc57a44d133828273edfabc2c77aec83851b
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-08 12:48:41 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,22 +7,31 @@ public class Main {
         DictionaryCommandline dic_com = new DictionaryCommandline();
         DictionaryManagement dic_manage = new DictionaryManagement();
         boolean Iscontinue = true;
+        boolean Isused = false;
         do {
             System.out.println("Menu:");
-            System.out.println("1: Insert from file, you can choose this function only once.");
+            System.out.println("1: Insert from file, you can choose this function only once");
             System.out.println("2: Show all words in this dictionary.");
             System.out.println("3: Insert from command line");
             System.out.println("4: Search an word by command line.");
             System.out.println("5: Delete an english word in this dictionary.");
-            System.out.println("6: Find all english world in this dictionary which have same prefix with s");
+            System.out.println("6: Find all english world in this dictionary ");
             System.out.println("7: Export this dictionay to file");
             System.out.println("8: Exit");
             System.out.println("Please select fuction [1-8]!");
 
             boolean Ispress = true;
+
             int select = in.nextInt();
             switch (select) {
-                case 1 -> dic_manage.insertFromFile();
+                case 1 -> {
+                    if (Isused) {
+                        System.out.println("Sorry u have insert already");
+                        break;
+                    }
+                    dic_manage.insertFromFile();
+                    Isused = true;
+                }
                 case 2 -> dic_com.showAllWords();
                 case 3 -> dic_manage.insertFromCommandline();
                 case 4 -> dic_manage.dictionaryLookup();
