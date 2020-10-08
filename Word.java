ba30a2e2200adbf3feb21b85f516b5ea59c7ec4a
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-08 10:02:37 +0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,47 +1,31 @@
 package Tu_dien;
 
-import java.util.ArrayList;
-
 public class Word {
-    private String word_target;
-    private ArrayList<String> word_explain;
-    //Constructors
-    public Word(String word_target, String word_explain) {
-        this.word_explain.add(word_explain);
+private String word_target;
+private String word_explained;
+    // Constructor
+    public Word(String word_target, String word_explained) {
+        this.word_target = word_target;
+        this.word_explained = word_explained;
+    }
+    public Word() {
+
+    }
+    /** Getter va setter cho word_target.*/
+    public void setWord_target(String word_target) {
         this.word_target = word_target;
     }
-    public Word(){
-    }
 
-    //Constructors
-
-    //Getter and Setter
-    public String getWord_target() {
-
+    public String getWord_target(){
         return word_target;
     }
-    public ArrayList<String> getWord_explain() {
-
-        return word_explain;
+    /** Getter va setter cho word_explained.*/
+    public void setWord_explained(String word_explained) {
+        this.word_explained = word_explained;
     }
-    public void setWord_target(String word_target) {
-
-        this.word_target = word_target;
-    }
-    public void setWord_explain(ArrayList<String> a) {
-
-        word_explain.addAll(a);
-    }
-    //Getter and Setter
-
-    //More functions
-    public void addWord_explain(String a) {
-        if(word_explain == null) word_explain = new ArrayList<>();
-        word_explain.add(a);
-    }
-    public void copy(Word a) {
-        this.setWord_explain(a.getWord_explain());
-        this.setWord_target(a.getWord_target());
+    public String getWord_explained() {
+        return word_explained;
     }
 
 }
+
