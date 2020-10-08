13e80d6aca4d2ad54562cfcd0650486d93e51a81
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-08 10:05:15 +0700

    Update DictionaryCommandline.java

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,69 +1,59 @@
 package Tu_dien;
 import java.util.*;
 import java.util.ArrayList;
-
 public class DictionaryCommandline {
-    private final Dictionary dictionary;
-    private final DictionaryManagement dict_manage;
-    private ArrayList<Word> list;
-
-    // Constructor
-    public DictionaryCommandline(Dictionary dictionary, DictionaryManagement dict_manage) {
-        this.dictionary = dictionary;
-        this.dict_manage = dict_manage;
-    }
-    // End Constructor
-    public void showAllWords() {
-        if(list == null) {
-            list = new ArrayList<>(dictionary.getList());
+    void showAllWords() {
+        int n = DictionaryManagement.dictionary.list.size();
+        System.out.printf("%-8s|%-40s|%s%n%n", "No", "English", "Vietnamese");
+        for(int i = 0; i < n; i++) {
+            System.out.printf("%-8s|%-40s|%s%n%n", i+1,
+                    DictionaryManagement.dictionary.list.get(i).getWord_target(),
+                    DictionaryManagement.dictionary.list.get(i).getWord_explained());
         }
-        boolean check_Empty = true;
-        int thu_tu = 0;
-        for(int i = 0; i < list.size(); i++) {
-            if (list.get(i) == null) continue;
-            for(int j = 0; j < list.get(i).getWord_explain().size(); j++) {
-                if (check_Empty) {
-                    check_Empty = false;
-                    String a = "No";
-                    String b = "English";
-                    String c = "Vietnamese";
-                    System.out.printf("%-8s|%-40s|%s%n%n", a, b, c);
-                }
-                System.out.printf("%-8d|%-40s|%s%n", ++thu_tu, list.get(i).getWord_target(), list.get(i).getWord_explain().get(j));
+    }
+
+    public void dictionaryBasic(){
+        DictionaryManagement.insertFromCommandline();
+        showAllWords();
+
+    }
+
+    public void dictionaryAdvanced() {
+        DictionaryManagement.insertFromFile();
+        showAllWords();
+        DictionaryManagement.dictionaryLookup();
+
+    }
+    public void dictionarySearcher() {
+        int n = DictionaryManagement.dictionary.list.size();
+        Scanner in = new Scanner(System.in);
+        boolean check = false;
+        System.out.println("What is the word you want to search ?");
+        ArrayList<Word> children = new ArrayList<>();
+        String input = in.nextLine();
+        for (int i = n; i < n; i++) {
+            String child = DictionaryManagement.dictionary.list.get(i).getWord_target()
+                    .substring(0, input.length());
+            if (child.equalsIgnoreCase(input)) {
+                check = true;
+                Word new_child = new Word(input, DictionaryManagement.dictionary.list
+                        .get(i).getWord_explained());
+
+                children.add(new_child);
             }
         }
-        if (check_Empty) {
-            System.out.println("There are no words in dictionary");
-        }
-        list = null;
-    }
-    public void dictionaryBasic() {
-        dict_manage.insertFromCommandline();
-        showAllWords();
-    }
-    public void dictionaryAdvanced() throws Exception {
-        dict_manage.insertFromFile();
-        showAllWords();
-        dict_manage.dictionaryLookup();
-    }
-    public void DictionarySearcher() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter to find : ");
-        String find = in.nextLine();
-        ArrayList<String> result;
-        result = dictionary.Searcher(find);
-        if (result == null) {
-            System.out.println("We cant find anything like that");
-        }
-        else if (result.size() == 0) {
-            System.out.println("We cant find anything like that");
+        if (check) {
+            System.out.println("We found " + children.size() + "word(s) :");
+            System.out.printf("%-8s|%-40s|%s%n%n", "No", "English", "Vietnamese");
+            for (int i = 0; i < children.size(); i++) {
+                System.out.printf("%-8s|%-40s|%s%n%n", i+1,
+                        children.get(i).getWord_target(),
+                        children.get(i).getWord_explained());
+            }
         }
         else {
-            System.out.println("We found" + result.size() + " results");
-            for(int i = 0; i < result.size(); i++) {
-                System.out.println(result.get(i));
-            }
+            System.out.println("Sorry we cant find that word");
         }
     }
-}
 
+}
