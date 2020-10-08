f47397d336f833797fb388ba368fda71fbb8b885
nkq232 <64514813+nkq232@users.noreply.github.com>
2020-10-08 12:47:03 +0700

    Update DictionaryManagement.java

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -10,19 +10,19 @@ public class DictionaryManagement {
         System.out.println("Nhap so tu ban muon input: ");
         int n;
         n = Integer.parseInt(in.nextLine());
-       int size = dictionary.list.size();   
+        int size = Dictionary.list.size();
         for(int i = 0; i < n; i++) {
-            dictionary.list.add(new Word());
+            Dictionary.list.add(new Word());
             System.out.println("Nhap tu thu " + (i + 1) + ":");
-            dictionary.list.get(i+size).setWord_target(in.nextLine());
-            dictionary.list.get(i+size).setWord_explained(in.nextLine());
+            Dictionary.list.get(i+size).setWord_target(in.nextLine());
+            Dictionary.list.get(i+size).setWord_explained(in.nextLine());
         }
     }
     public static void insertFromFile() {
         dictionary = new Dictionary();
         try {
 
-            File f = new File("dictionaries.txt");
+            File f = new File("C:\\Users\\Admin\\IdeaProjects\\BTL\\src\\Tu_dien\\dictionaries.txt");
             FileReader fr = new FileReader(f);
 
             BufferedReader br = new BufferedReader(fr);
@@ -31,8 +31,8 @@ public class DictionaryManagement {
             for(int i = 0;(s = br.readLine()) != null ; ++i){
                 String[] word = s.split("\\t");
                 dictionary.add();
-                dictionary.list.get(i).setWord_target(word[0]);
-                dictionary.list.get(i).setWord_explained(word[1]);
+                Dictionary.list.get(i).setWord_target(word[0]);
+                Dictionary.list.get(i).setWord_explained(word[1]);
 
             }
             fr.close();
@@ -43,13 +43,14 @@ public class DictionaryManagement {
     }
     public static void dictionaryLookup() {
         Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
-        int n = dictionary.list.size();
         System.out.println("Input the word you want to find: ");
+        String a = in.nextLine();
+        int n = Dictionary.list.size();
+
         boolean check = false;
         for (int i = 0; i < n; i++) {
-            if (a.equalsIgnoreCase(dictionary.list.get(i).getWord_target())) {
-                System.out.println(dictionary.list.get(i).getWord_explained());
+            if (a.equalsIgnoreCase(Dictionary.list.get(i).getWord_target())) {
+                System.out.println(Dictionary.list.get(i).getWord_explained());
                 check = true;
             }
         }
@@ -59,15 +60,15 @@ public class DictionaryManagement {
     }
     public static void Edit() {
         Scanner in = new Scanner(System.in);
-        int n = dictionary.list.size();
+        int n = Dictionary.list.size();
         boolean check = false;
         System.out.println("What is the target word u want to edit: ");
         String En_before = in.nextLine();
         for (int i = 0; i < n ; i++) {
-            if (En_before.equalsIgnoreCase(dictionary.list.get(i).getWord_target())) {
+            if (En_before.equalsIgnoreCase(Dictionary.list.get(i).getWord_target())) {
                 System.out.println("Input the new target word and explained word: ");
-                dictionary.list.get(i).setWord_target(in.nextLine());
-                dictionary.list.get(i).setWord_explained(in.nextLine());
+                Dictionary.list.get(i).setWord_target(in.nextLine());
+                Dictionary.list.get(i).setWord_explained(in.nextLine());
                 check = true;
             }
         }
@@ -82,12 +83,20 @@ public class DictionaryManagement {
     public static void delete() {
         Scanner in = new Scanner(System.in);
         boolean check = false;
-        int n = dictionary.list.size();
+        int n = Dictionary.list.size();
         System.out.println("What is the target word u want to delete: ");
         String En_before = in.nextLine();
+
         for (int i = 0; i < n; i++) {
-            if (En_before.equalsIgnoreCase(dictionary.list.get(i).getWord_target())) {
-                dictionary.list.remove(i);
+            String checker = Dictionary.list.get(i).getWord_target();
+            if (En_before.equalsIgnoreCase(checker)) {
+                Word remove = Dictionary.list.get(i);
+                Word last = Dictionary.list.get(n-1);
+                Word mid_change = new Word();
+                mid_change.copy_value(remove);
+                remove.copy_value(last);
+                last.copy_value(mid_change);
+                Dictionary.list.remove(n-1);
                 check = true;
             }
         }
@@ -100,15 +109,16 @@ public class DictionaryManagement {
     }
     public static void dictionaryExportToFile() throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("Export.txt"));
-        int n = dictionary.list.size();
+        int n = Dictionary.list.size();
         for (int i = 0; i < n; i++) {
-                String word_target = dictionary.list.get(i).getWord_target();
-                String word_explain = dictionary.list.get(i).getWord_explained();
-                writer.write(word_target + "\t" + word_explain);
-                writer.newLine();
+            String word_target = Dictionary.list.get(i).getWord_target();
+            String word_explain = Dictionary.list.get(i).getWord_explained();
+            writer.write(word_target + "\t" + word_explain);
+            writer.newLine();
 
         }
         writer.close();
     }
 }
+
 
